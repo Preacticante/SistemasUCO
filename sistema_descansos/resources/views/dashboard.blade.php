3c20d7eb058d20c2cc74fa30686a28b3e060891b
Preacticante <practicante.tie@prepauco.edu.mx>
2026-05-22 14:22:04 -0600

    logo institucional
    se corrigieron errores y se agrego el logo en el pdf y en la pantalla principal

--- sistema_descansos/resources/views/dashboard.blade.php
+++ sistema_descansos/resources/views/dashboard.blade.php
@@ -9,6 +9,18 @@
 @section('content')
     @include('dashboard.navbar')
 
+    @php
+            $path = public_path('img/logo_uco.png');
+            $type = pathinfo($path, PATHINFO_EXTENSION);
+            $data = file_get_contents($path);
+            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        @endphp
+
+<div style="position: absolute; left: 50px; top: 75px; z-index: 10;">
+        <img src="{{ $base64 }}" 
+             alt="UCO PREPA CONTEMPORÁNEA"
+             style="height: 150px;"> </div>
+
     <div class="container">
         <h1>Panel Principal</h1>
 
--- sistema_descansos/storage/framework/views/f03d7d14fd3f4db17f89ca123994ffbc.php
+++ sistema_descansos/storage/framework/views/f03d7d14fd3f4db17f89ca123994ffbc.php
@@ -7,6 +7,18 @@
 <?php $__env->startSection('content'); ?>
     <?php echo $__env->make('dashboard.navbar', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
 
+    <?php
+            $path = public_path('img/logo_uco.png');
+            $type = pathinfo($path, PATHINFO_EXTENSION);
+            $data = file_get_contents($path);
+            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        ?>
+
+<div style="position: absolute; left: 50px; top: 75px; z-index: 10;">
+        <img src="<?php echo e($base64); ?>" 
+             alt="UCO PREPA CONTEMPORÁNEA"
+             style="height: 150px;"> </div>
+
     <div class="container">
         <h1>Panel Principal</h1>
 
