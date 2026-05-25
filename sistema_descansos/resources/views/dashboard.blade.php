e7921a08ff8f5067f6bdb6c6afb7dbfe56b39728
Preacticante <practicante.tie@prepauco.edu.mx>
2026-05-25 08:48:15 -0600

    logo institucional
    se agrego el logo institucional en las pantallas de vacaciones y principal

--- sistema_descansos/resources/views/dashboard.blade.php
+++ sistema_descansos/resources/views/dashboard.blade.php
@@ -10,18 +10,21 @@
     @include('dashboard.navbar')
 
     @php
-            $path = public_path('img/logo_uco.png');
-            $type = pathinfo($path, PATHINFO_EXTENSION);
-            $data = file_get_contents($path);
-            $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-        @endphp
+        $path = public_path('img/logo_uco.png');
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+    @endphp
 
-<div style="position: absolute; left: 50px; top: 75px; z-index: 10;">
-        <img src="{{ $base64 }}" 
-             alt="UCO PREPA CONTEMPORÁNEA"
-             style="height: 150px;"> </div>
+    <div style="max-width: 1200px; margin: 0 auto; padding: 20px 15px; position: relative;">
+        
+        <div style="position: absolute; left: 150px; top: 55px;">
+            <img src="{{ $base64 }}" 
+                 alt="UCO PREPA CONTEMPORÁNEA"
+                 style="height: 150px; width: auto;">
+        </div>
 
-    <div class="container">
+    <div class="container" style="margin-top: 40px;">
         <h1>Panel Principal</h1>
 
         @include('dashboard.stat-cards')
